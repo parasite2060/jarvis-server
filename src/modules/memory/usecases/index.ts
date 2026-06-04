5d31d03f77bb0bcd95f56f8e6c6003c452bf1ea6
tannt-mti <tannt@mti.techvn.co.jp>
2026-06-04 20:00:51 +0700

    refactor(memory): remove MemU-backed search/add routes and usecases

--- src/modules/memory/usecases/index.ts
+++ src/modules/memory/usecases/index.ts
@@ -1,7 +1,5 @@
-import { SearchMemoryUseCase } from './search-memory.usecase';
-import { AddMemoryUseCase } from './add-memory.usecase';
 import { GetSoulUseCase } from './get-soul.usecase';
 import { GetIdentityUseCase } from './get-identity.usecase';
 import { GetMemoryFileUseCase } from './get-memory-file.usecase';
 
-export const UseCases = [SearchMemoryUseCase, AddMemoryUseCase, GetSoulUseCase, GetIdentityUseCase, GetMemoryFileUseCase];
+export const UseCases = [GetSoulUseCase, GetIdentityUseCase, GetMemoryFileUseCase];
